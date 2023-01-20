0285486146f6f3630fbe3f56480eac7dfe864477
Michael Mitchell <nihongodehanaseru@yahoo.com>
2023-01-20 00:30:42 -0500

    Added the readme text and the 1st .js commit

---
+++ README.txt
@@ -0,0 +1,28 @@
+This is my first javaScript app.
+
+Project's Title =============
+The Name of this project is Dad Joke generator....
+Project Description =============
+This project will supply you with a random dad joke until you've had enough.
+Include Credits ==============
+The logic of this app was created by Michael Mitchell and the dad Jokes were supplied by https://www.goodhousekeeping.com/life/parenting/a36279135/best-corny-dad-jokes-for-kids/
+
+Copyright (c) <2023> <Michael Mitchell>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ dad_joke_gen.js
@@ -0,0 +1,44 @@
+function sleep(miliseconds) {
+  var currentTime = new Date().getTime();
+
+  while (currentTime + miliseconds >= new Date().getTime()) {}
+}
+
+const dad_joke = {
+  "I have a joke about chemistry": "but I don’t think it’ll get a reaction",
+  "I have a joke about banking": "but I lost interest.",
+  "I have a joke about cows": "but I don't want to milk it.",
+  "I have a joke about kites": "but it would just sail over your head.",
+  "I have a scary joke about math": "but I'm 2² to say it.",
+  "I have a joke about construction": "but I’m still working on it.",
+  "I have a joke about time travel": "but you guys didn’t get it.",
+  "I have a joke about being an electrician": "but it’s too shocking.",
+  "I have a joke about hunting for fossils":
+    "but you probably wouldn’t dig it.",
+  "I have a joke about a broken pencil": "but it’s pointless.",
+  "I have a joke about the flu": "but I hope you don’t get it.",
+  "I have a joke about statistics": "but it’s not significant.",
+  "I have a joke about pizza": "but it’s too cheesy.",
+  "I have a joke about immortality": "and it never gets old.",
+  "I have a joke about paper": "but it’s tearable.",
+  "I have a joke about trickle-down economics":
+    "but 99% of you will never get it.",
+  "I have a joke about drilling": "but it’s boring.",
+  "I have a joke about being a rejected organ donor":
+    "but I just don't have the guts.",
+  "I had a joke about canned juice": "but I couldn't concentrate.",
+  "I have a few jokes about retired people": "but none of them work.",
+  "I have a joke about a broken clock": "but it’s not the right time.",
+  "I have a joke about butter": "but I’m not going to spread it.",
+  "I have a joke about a roof": "but it would just go over your head.",
+  "I have a joke about inferiority complexes": "but it’s not very good.",
+  "I have a joke about procrastination": "but I’ll tell it to you later.",
+};
+
+let rando = Math.floor(Math.random() * Object.keys(dad_joke).length);
+console.log(rando);
+console.log(Object.keys(dad_joke).length);
+
+console.log(Object.keys(dad_joke)[rando].toString());
+sleep(2000);
+console.log(dad_joke[Object.keys(dad_joke)[rando]]);
